4318ac04d3ff9ccd95cfe71781d8aad1c805152a
Priyadarshi Peter <priyadarshi035@gmail.com>
2024-01-23 13:04:30 +0000

    Updated Script

--- sbm.sh
+++ sbm.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 #Declaring an array to receive comma separated Service bus names as an input.
-declare -a SERVICEBUS_NAMES
+#declare -a SERVICEBUS_NAMES
 
 SERVICEBUS_NAMES=$1
 AZURE_RESOURCEGROUP_NAME=""
@@ -12,27 +12,28 @@ TENANTID=""
 
 
 #Login into Azure Subscription.
-az login --service-principal -u $SERVICEPRINCIPALID -p $SERVICEPRINCIPALKEY --tenant $TENANTID
+az login --service-principal -u $SERVICEPRINCIPALID -p $SERVICEPRINCIPALKEY --tenant $TENANTID >/dev/null
 TOKEN=$(az account get-access-token --resource=https://servicebus.azure.net --query accessToken | tr -d '"')
 
 
 purge_dlq_queue() {
-    local DLQ_QUEUE_URL="https://${SERVICENAMESPACE}.servicebus.windows.net/tax-report-nf-params-lines/\$DeadLetterQueue/messages/head"
+    local DLQ_QUEUE_URL="https://${SERVICEBUS_NAMES}.servicebus.windows.net/$1/\$DeadLetterQueue/messages/head"
     local count=1000
-    echo "cleaning the dead letters messages from the message queue..."
+    echo "INFORM: Cleaning the dead letters messages from the message queue..."
     while [[ ${count} -ge 0 ]]
     do
-        local STATUS_CODE=$(curl -I -X DELETE -H "Authorization: Bearer $TOKEN" ${DLQ_QUEUE_URL} 2>/dev/null | head -n 1 | cut -d$' ' -f2)
-        if [[ STATUS_CODE -ge 300 ]]; then
-            echo "Exit dead letters message queue cleaning with code ${STATUS_CODE}"
+	local STATUS_CODE=$(curl -I -X DELETE -H "Authorization: Bearer $TOKEN" ${DLQ_QUEUE_URL} 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+	
+	if [[ STATUS_CODE -ge 300 ]]; then
+            echo "ERROR: Exit dead letters message queue cleaning with code ${STATUS_CODE}"
             return 1
         elif [[ STATUS_CODE -eq 204 ]]; then
-            echo "dead letters message queue has been cleaned"
+            echo "INFORM: All Messages from Dead letters message queue $1 has been cleaned"
             return 0
         fi
         let count--
     done
-    echo "Exit with maxium number tries."
+    echo "ERROR: Exit with maxium number tries."
     return 1
 }
 
@@ -66,9 +67,13 @@ Queues_Details () {
             DLQ_QUEUE_COUNT=$(echo $j | jq .countDetails.deadLetterMessageCount)
             
             echo "Service Bus Name --> $1; DLQ Queue Name --> $DLQ_QUEUE_NAME; DLQ Message Count --> $DLQ_QUEUE_COUNT" >> MSOps_SB_DLQ_Details-$(date '+%d-%m-%Y').txt
-            #check for the commands
-            purge_dlq_queue $DLQ_QUEUE_NAME
-            #az servicebus queue purge --resource-group $AZURE_RESOURCEGROUP_NAME --namespace-name $1 --queue-name $DLQ_QUEUE_NAME
+        done
+       echo $DEAD_LETTER_QUEUE | jq -c '.[]' | while read k;
+	do
+	DLQ_QUEUE_NAME=$(echo $k | jq .name | tr -d '"')
+	
+	  purge_dlq_queue $DLQ_QUEUE_NAME
+            
             if [ $? -ne 0 ] 
             then
                 echo "ERROR: The Deletion of Messages from DEAD LETTER QUEUE Failed!!!"
